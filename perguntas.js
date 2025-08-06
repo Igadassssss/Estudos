63d6900871f8b4f71dfcddb7e3b0474bd1778767
Igadassssss <maciel.vieira@escola.pr.gov.br>
2025-08-06 09:49:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,10 @@
+criaCartao(
+    "Matemática",
+    "O número 11 é um número primo?",
+    "Sim, pois ele só existe na tabuada do 1 e dele mesmo"
+)
+criaCartao(
+    "Matemática",
+    "Quanto é 6!",
+    "720",
+)
