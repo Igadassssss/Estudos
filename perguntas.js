04c0d820200b266dcb2d242ca257f4c5fed0087f
Igadassssss <maciel.vieira@escola.pr.gov.br>
2025-08-08 09:50:19 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,10 +1,30 @@
 criaCartao(
-    "Matemática",
-    "O número 11 é um número primo?",
-    "Sim, pois ele só existe na tabuada do 1 e dele mesmo"
+    "Pergunta 1:",
+    "Que horas são agora?",
+    "It's TV-Time"
 )
 criaCartao(
-    "Matemática",
-    "Quanto é 6!",
-    "720",
+    "Pergunta 2:",
+    "Qual o nome do companheiro de Susie? (Eu não lembro o nome dele)",
+    "Lancer",
+)
+criaCartao(
+    "Pergunta 3:",
+    "Qual o nome do apresentador?",
+    "Mr. Tenna",
+)
+criaCartao(
+    "Pergunta 4:",
+    "Quanto custa uma pelúcia do Ralsei?",
+    "$ 32,00",
+)
+criaCartao(
+    "Pergunta 5:",
+    "Qual a comida favorita de Kris?",
+    "Torta",
+)
+criaCartao(
+    "Pergunta 6:",
+    "Qual o personagem favorito de Asriel?",
+    "Mago Espadachim",
 )
